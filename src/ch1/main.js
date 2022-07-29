86987d01780180e6dbcc0957f81301073c96ae8d
morganbergen <morgan@ku.edu>
2022-07-29 13:03:48 -0500

    working on primative data types

---
+++ src/ch1/main.js
@@ -0,0 +1,95 @@
+// Language: javascript
+
+let x = 2;
+let y = 3;
+
+console.log(x + y);
+console.log((x === 2) && (y === 3));
+console.log((x > 3 || (y < 3)));
+
+x = 0;
+console.log(x);
+
+// primitive data types
+integer_var = 1000;
+double_var = 0.4;
+string_var = "string in quotes";
+delimit_str_var = 'delimit string'
+boolean_var = true;
+boolean_var0 = false;
+nothing = null;
+undefined_var = undefined;
+
+console.log(integer_var);
+console.log(double_var);
+console.log(string_var);
+console.log(delimit_str_var);
+console.log(boolean_var);
+console.log(boolean_var0);
+console.log(nothing);
+console.log(undefined_var);
+
+
+console.log("foo".includes.apply("f"));
+
+// standard built-in objects
+/*
+objects
+arrays
+
+note that arrays are not primative data types unlike in C++
+*/
+
+// an object is a collection of name/value pairs, or a string to value map
+let morgan = {
+    first: "Morgan",
+    middle: "Maha",
+    last: "Bergen",
+    species: "homo sapien",
+    age: 24,
+    sex: 'female',
+    education: "University of Kansas",
+    degree: "BS, Computer Science",
+    class_status: "Junior",
+    birth_date: "March 24, 1998",
+    hometown: "Wichita, Kansas USA",
+    nationality: "American",
+    race: "white & middle eastern",
+    religion: "pantheism",
+    language: "english",
+    economic_philosophy: "Libertarianism",
+    political_philosophy: "Progressism",
+    belief_system: "Pragm atic Optimism",
+    values: ["An Independent Mind", "Good Health", "Unadultured Curosity", "Meaningful Work", "Financial Freedom", "Emotional Intelligence", "Humor", "Love"],
+    mother: "Mona Al-Abed Bergen",
+    father: "David Bruce Bergen",
+    brother: "David Khaled Bergen",
+}
+
+
+// access the properties of the object morgan with a . operator or [] operator:
+console.log(morgan.first);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
