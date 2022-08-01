27ce9facdc1a5cd7250811b316123c9ff5747052
morganbergen <morgan@ku.edu>
2022-08-01 11:40:23 -0500

    notes will be provided in MD files for now on

---
+++ src/3-types-values-variables/primatives.js
@@ -0,0 +1,38 @@
+/*
+
+
+#### Arithmetic in Javascript
+
+- basic arithmetic
+< + > // addition
+< - > // subtraction
+< * > // multiplication
+< ** > // exponentiation
+< / > // division
+< // > // integer division
+< % > // modulus
+< ++ > // increment
+< -- > // decrement
+
+- complex arithmetic
+< Math.pow(2, 53) > // => 9007199254740992: 2 to the power of 53
+< Math.round(.6) > // =>
+
+
+
+*/
+
+let power = Math.pow(2, 53);
+let round = Math.round(.6);
+let ceiling = Math.ceil(.6);
+let floor = Math.floor(.6);
+let absolute = Math.abs(-5);
+
+function show() {
+    console.log(power);
+    console.log(round);
+}
+
+show();
+
+
